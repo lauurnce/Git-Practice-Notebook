37c2b4080d382b0dab2fa72f5365620ccccf74de
Your Full Name <paneslawrence8@gmail.com>
2025-10-27 23:13:16 +0800

    change it to master

--- React-Practice/react-native-hackathon-app/src/App.tsx
+++ React-Practice/react-native-hackathon-app/src/App.tsx
@@ -8,7 +8,7 @@ const Stack = createStackNavigator();
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
+      <Stack.Navigator initialRouteName="Master">
         <Stack.Screen name="Home" component={HomeScreen} />
       </Stack.Navigator>
     </NavigationContainer>
