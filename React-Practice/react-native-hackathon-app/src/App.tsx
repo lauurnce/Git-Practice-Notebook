06ddcb96a2cd8163534380be30f02ecaf8b1e00f
Your Full Name <paneslawrence8@gmail.com>
2025-10-27 23:19:24 +0800

    turn it back to home

--- React-Practice/react-native-hackathon-app/src/App.tsx
+++ React-Practice/react-native-hackathon-app/src/App.tsx
@@ -9,7 +9,7 @@ const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Master">
-        <Stack.Screen name="Master" component={HomeScreen} />
+        <Stack.Screen name="Home" component={HomeScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
