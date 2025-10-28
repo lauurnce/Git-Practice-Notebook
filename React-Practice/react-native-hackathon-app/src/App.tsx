f7f07828d31ffbca004c882fc6ca11dc05e51203
Your Full Name <paneslawrence8@gmail.com>
2025-10-28 20:27:25 +0800

    homescreen

--- React-Practice/react-native-hackathon-app/src/App.tsx
+++ React-Practice/react-native-hackathon-app/src/App.tsx
@@ -8,7 +8,7 @@ const Stack = createStackNavigator();
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="home">
+      <Stack.Navigator initialRouteName="homescreen">
         <Stack.Screen name="Home" component={HomeScreen} />
       </Stack.Navigator>
     </NavigationContainer>
