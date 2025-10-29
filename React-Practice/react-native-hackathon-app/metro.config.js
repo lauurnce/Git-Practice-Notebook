afa67313dfcede0e8f04d36927ea9b2b6496133b
Your Full Name <paneslawrence8@gmail.com>
2025-10-29 22:33:46 +0800

    true

--- React-Practice/react-native-hackathon-app/metro.config.js
+++ React-Practice/react-native-hackathon-app/metro.config.js
@@ -2,7 +2,7 @@ module.exports = {
   transformer: {
     getTransformOptions: async () => ({
       transform: {
-        experimentalImportSupport: true,
+        experimentalImportSupport: false,
         inlineRequires: false,
       },
     }),
