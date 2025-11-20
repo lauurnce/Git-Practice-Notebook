92452a264519a480e83ceffb75e4ad6eb27ef3e3
Your Full Name <paneslawrence8@gmail.com>
2025-11-20 20:30:44 +0800

    awdawa

--- group4.cob
+++ group4.cob
@@ -12,8 +12,8 @@
        OBJECT-COMPUTER.    IBM-PC.  
        INPUT-OUTPUT SECTION.  
        FILE-CONTROL.  
-           SELECT INFILE ASSIGN TO ‘SALES.txt’.  
-           SELECT OUTFILE ASSIGN TO ‘BENTA’.  
+           SELECT INFILE ASSIGN TO 'SALES.txt'.  
+           SELECT OUTFILE ASSIGN TO 'BENTA'.      
        DATA DIVISION.  
        FILE SECTION.  
        FD INFILE  
@@ -21,85 +21,81 @@
            RECORD CONTAINS 38 CHARACTERS 
            DATA RECORD IS INREC.  
        01  INREC.  
-           02        AC    PIC  X.  
-           02        SNO   PIC  9(5).  
-           02        SNA.   
+           02 AC    PIC  X.  
+           02 SNO   PIC  9(5).  
+           02 SNA.   
                 05 FN PIC X(10). 
                 05 MN PIC X(5). 
                 05 LN PIC X(10). 
- 
- 
            02        AMT  PIC  9(5)V99.  
+
        FD  OUTFILE  
            LABEL RECORD IS OMITTED  
            DATA RECORD IS OUTREC.  
        01 OUTREC.  
            02   FILLER PIC X(80).  
+
        WORKING-STORAGE SECTION.  
        01  SVAC           PIC   X    VALUE SPACES.  
        01  TNS            PIC   9(4)  VALUE ZERO.  
        01  TA             PIC   9(7)V99   VALUE ZERO.  
        01  EOFSW          PIC   9  VALUE ZERO.  
        01  HEAD-1.  
-           02  FILLER  PIC X (29)    VALUE  SPACES.  
-           02  FILLER  PIC X(22)     VALUE ‘SAN MIGUEL 
-           CORPORATION’. 
-           02  FILLER  PIC X (29)    VALUE SPACES.  
+           02  FILLER  PIC X(29)    VALUE  SPACES.  
+           02  FILLER  PIC X(22)     VALUE 'SAN MIGUEL CORPORATION'. 
+           02  FILLER  PIC X(29)    VALUE SPACES.  
        01  HEAD-2.  
-           02  FILLER  PIC X (31)    VALUE SPACES.  
-           02  FILLER  PIC X (17)    VALUE  ‘STA.MESA, 
-           MANILA’. 
+           02  FILLER  PIC X(31)    VALUE SPACES.  
+           02  FILLER  PIC X(17)    VALUE  'STA.MESA, MANILA'. 
            02   FILLER PIC X(32)     VALUE SPACES.  
        01  SUB-1.  
            02 FILLER PIC X(34)        VALUE SPACES.  
-           02 FILLER PIC X (12)       VALUE ‘SALES REPORT’. 
+           02 FILLER PIC X(12)       VALUE 'SALES REPORT'. 
            02 FILLER PIC X(34)        VALUE SPACES.  
        01  SUB-2.  
            02 FILLER PIC X(7)         VALUE SPACES.  
-           02 FILLER PIC X(4)         VALUE ‘AREA’.  
+           02 FILLER PIC X(4)         VALUE 'AREA'.  
            02 FILLER PIC X(7)         VALUE SPACES.  
-           02 FILLER PIC X(8)         VALUE ‘SALESMAN’.  
+           02 FILLER PIC X(8)         VALUE 'SALESMAN'.  
            02 FILLER PIC X(16)        VALUE SPACES.  
-           02 FILLER PIC X(8)         VALUE ‘SALESMAN’.  
+           02 FILLER PIC X(8)         VALUE 'SALESMAN'.  
            02 FILLER PIC X(15)        VALUE SPACES.  
-           02 FILLER PIC X(6)         VALUE ‘AMOUNT’.  
+           02 FILLER PIC X(6)         VALUE 'AMOUNT'.  
            02 FILLER PIC X(9)         VALUE SPACES.  
        01  SUB-3.  
            02 FILLER PIC X(7)         VALUE SPACES.  
-           02 FILLER PIC X(4)         VALUE ‘CODE’.  
+           02 FILLER PIC X(4)         VALUE 'CODE'.  
            02 FILLER PIC X(8)         VALUE SPACES.  
-           02 FILLER PIC X(6)         VALUE ‘NUMBER’.  
+           02 FILLER PIC X(6)         VALUE 'NUMBER'.  
            02 FILLER PIC X(18)        VALUE SPACES.  
-           02 FILLER PIC X(4)         VALUE ‘NAME’.  
+           02 FILLER PIC X(4)         VALUE 'NAME'.  
            02 FILLER PIC X(33)        VALUE SPACES.  
        01  DETALYE.  
            02 FILER PIC X(9)          VALUE SPACES.  
            02 P-AC PIC X.  
-           02 FILLER PIC X (10)       VALUE SPACES.  
+           02 FILLER PIC X(10)       VALUE SPACES.  
            02 P-SNO PIC 9(5).  
-           02 FILLER PIC X (8)        VALUE SPACES.  
-           02 P-SNA PIC X (25).  
-           02 FILLER PIC X(7)         VALUES SPACES.  
+           02 FILLER PIC X(8)        VALUE SPACES.  
+           02 P-SNA PIC X(25).  
+           02 FILLER PIC X(7)        VALUES SPACES.  
            02 P-AMT PIC 99,999.99.  
            02 FILLER PIC X(6)         VALUE SPACES.  
        01  TOTAL-1.  
-           02 FILLER PIC X (7)        VALUE SPACES.  
-           02 FILLER PIC X (9)        VALUE ‘TOTAL NO.’.  
-           02 FILLER PIC X (12)       VALUE ‘OF SALESMEN:’.  
-           02 FILLER PIC X (9)        VALUE SPACES.  
+           02 FILLER PIC X(7)        VALUE SPACES.  
+           02 FILLER PIC X(9)        VALUE 'TOTAL NO.'.  
+           02 FILLER PIC X(12)       VALUE 'OF SALESMEN:'.  
+           02 FILLER PIC X(9)        VALUE SPACES.  
            02 P-TNS PIC 9,999.  
-           02 FILLER PIC X (38)       VALUE SPACES.  
+           02 FILLER PIC X(38)       VALUE SPACES.  
        01  TOTAL-2.  
-           02 FILLER PIC X (7)        VALUE SPACES.  
-           02 FILLER PIC X (6)        VALUE ‘TOTAL ’.  
-           02 FILLER PIC X (11)       VALUE ‘ACCUMULATED’. 
-           02 FILLER PIC X (8)        VALUE ‘ AMOUNT:’.  
-           02 FILLER PIC X(4)         VALUE ‘ PHP’.  
+           02 FILLER PIC X(7)        VALUE SPACES.  
+           02 FILLER PIC X(6)        VALUE 'TOTAL '.  
+           02 FILLER PIC X(11)       VALUE 'ACCUMULATED'. 
+           02 FILLER PIC X(8)        VALUE 'AMOUNT:'.  
+           02 FILLER PIC X(4)         VALUE 'PHP'.  
            02 P-TA PIC 9,999,999.99.  
-           02 FILLER PIC X (33)        VALUE SPACES.  
-       SCREEN SECTION.  
-       01 SCRE.  
-           02 BLANK SCREEN.  
+           02 FILLER PIC X(33)        VALUE SPACES.  
+        
        PROCEDURE DIVISION.  
        MAIN-RTN.  
            PERFORM INIT-RTN THRU INIT-RTN-END.  
@@ -115,7 +111,7 @@
        INIT-RTN-END.
        END-RTN.  
            MOVE 1 TO EOFSW.  
-           DISPLAY ‘EMPTY FILE’ LINE 3 COLUMN 20.  
+           DISPLAY 'EMPTY FILE' LINE 3 COLUMN 20.  
        HEADING-RTN.  
            WRITE OUTREC FROM HEAD-1 AFTER PAGE.  
            WRITE OUTREC FROM HEAD-2 AFTER 1. 
@@ -123,7 +119,7 @@
            WRITE OUTREC FROM SUB-2 AFTER 2.  
            WRITE OUTREC FROM SUB-3 AFTER 1.  
        PROCESS-RTN.  
-           DISPLAY SCRE.  
+           DISPLAY ''.  
            IF SVAC NOT = AC PERFORM AC-BREAK-RTN.  
            MOVE AC TO P-AC.  
            MOVE SNO TO P-SNO.  
@@ -143,4 +139,4 @@
            MOVE AC TO SVAC.  
        FINISH-RTN.  
            CLOSE INFILE, OUTFILE.  
-           DISPLAY ‘TAPOS NA’ LINE 6 COLUMN 20. 
+           DISPLAY 'TAPOS NA'. LINE 6 COLUMN 20. 
