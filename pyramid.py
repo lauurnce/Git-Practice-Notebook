d2c0bd731b285b68bb85d829e01838ca09a434fc
Your Full Name <paneslawrence8@gmail.com>
2025-12-01 23:06:44 +0800

    Array in COBOL

--- pyramid.py
+++
@@ -1,8 +0,0 @@
-def print_pyramid(height):
-    for i in range(1, height + 1):
-        spaces = ' ' * (height - i)
-        stars = '*' * (2 * i - 1)
-        print(spaces + stars + spaces)
-
-# Example usage:
-print_pyramid(5)
